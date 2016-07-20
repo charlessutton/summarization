e474d661e7c2e0ee7e125c7aea8ba6d6c7470107
charles <charles@ybcdata.com>
2016-07-20 09:04:41 +0300

    Improvement of the patch_to_json.py (counter added)

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 13 23:42:43 2016
+
+@author: Code
+"""
+
---
+++ misc/sanstitre0.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 13 23:28:23 2016
+
+@author: Code
+"""
+
+# imports 
+
+
+
+# folders
+
+
+# open a patch
+
+
+
+# check if a title is in the the corresponding done list
+# try to download the page 
+# if there is a problem write in the error list with error
+# if no write a json file with and add the title to the done list
