8b23226abd48bd8f1867750cf2e9b26700ad0ba9
Ubuntu <ubuntu@ip-172-31-3-170.us-west-1.compute.internal>
2016-10-09 12:21:51 +0000

    suppression des fichiers supprimés

--- misc/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 11 23:46:48 2016
-
-@author: Code
-"""
-
--- parser/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul  8 18:11:32 2016
-
-@author: Charles Sutton
-"""
